718706c6de2d81108b433151c9198ce5bf67eb4a
ColeSpletstoser <cole.spletstoser@drake.edu>
2016-10-19 23:01:57 -0500

    Initial Commit
    Added initial files

---
+++ DoggyDate/app/src/main/java/cs188/doggydate/HomePage.java
@@ -0,0 +1,13 @@
+package cs188.doggydate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
