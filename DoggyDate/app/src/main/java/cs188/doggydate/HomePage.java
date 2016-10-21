82c6742a9526b5cc4c00991533496d721c7464eb
ColeSpletstoser <cole.spletstoser@drake.edu>
2016-10-20 21:09:49 -0500

    Created ViewPager
    Created the main view pager with the home screen, profile/settings
    screen, and invite screen

--- DoggyDate/app/src/main/java/cs188/doggydate/HomePage.java
+++ DoggyDate/app/src/main/java/cs188/doggydate/HomePage.java
@@ -1,13 +1,52 @@
 package cs188.doggydate;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomePage extends AppCompatActivity {
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
+    }
+
+    private class CustomAdapter extends FragmentPagerAdapter {
+        public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    if(true) //change to check for if the user has an account
+                        return new ProfileFragment();
+                    else
+                        return new LogInFragment();
+                case 1:
+                    return new HomeFragment();
+                case 2:
+                    return new InviteFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 }
---
+++ DoggyDate/app/src/main/java/cs188/doggydate/InviteFragment.java
@@ -0,0 +1,19 @@
+package cs188.doggydate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Spletz on 10/20/16.
+ */
+public class InviteFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.invite_fragment, container, false);
+    }
+}
