ba670c5c48b7c407320e5ce7e1e6d0cabac7be03
truewheeldon <truewheeldon@gmail.com>
2016-10-22 16:28:14 -0500

    Working on buttons

--- DoggyDate/app/src/main/java/cs188/doggydate/ProfileFragment.java
+++ DoggyDate/app/src/main/java/cs188/doggydate/ProfileFragment.java
@@ -1,19 +1,42 @@
 package cs188.doggydate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by Spletz on 10/20/16.
+ * edited by True 10/22/16
  */
-public class ProfileFragment extends Fragment {
-    @Nullable
+
+
+public class ProfileFragment extends Fragment implements View.OnClickListener {
+
+    ImageButton btn;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        btn = (ImageButton) view.findViewById(R.id.settingsButton);
+        btn.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View v)
+    {
+//        Intent myIntent = new Intent(ProfileFragment.this, SettingsActivity.class);
+//        //myIntent.putExtra("key", value); //Optional parameters
+//        ProfileFragment.this.startActivity(myIntent);
+   }
 }
--- DoggyDate/app/src/main/java/cs188/doggydate/SettingsActivity.java
+++ DoggyDate/app/src/main/java/cs188/doggydate/SettingsActivity.java
@@ -2,14 +2,20 @@ package cs188.doggydate;
 
 //created by True Wheeldon 10/22/2016
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_settings);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key"); //if it's a string you stored.
     }
 }
