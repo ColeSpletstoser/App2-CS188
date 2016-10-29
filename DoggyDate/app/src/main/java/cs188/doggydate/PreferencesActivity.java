9059f8edbdc7bf7691a4a6430d470bf4c69d98da
truewheeldon <truewheeldon@gmail.com>
2016-10-29 16:33:58 -0500

    Settings/Profile edits
    PreferenceActivity now properly updates the textviews on
    ProfileFragment. dummy images have been put in imageviews to be later
    replaced by upload from kelly's code

--- DoggyDate/app/src/main/java/cs188/doggydate/PreferencesActivity.java
+++ DoggyDate/app/src/main/java/cs188/doggydate/PreferencesActivity.java
@@ -173,14 +173,20 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
-            setHasOptionsMenu(true);
+            //setHasOptionsMenu(true);
 
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
-            bindPreferenceSummaryToValue(findPreference("example_list"));
+            //bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("dog_name"));
+            bindPreferenceSummaryToValue(findPreference("dog_age"));
+            bindPreferenceSummaryToValue(findPreference("dog_breed"));
+            bindPreferenceSummaryToValue(findPreference("dog_gender"));
+            bindPreferenceSummaryToValue(findPreference("dog_description"));
+            bindPreferenceSummaryToValue(findPreference("owner_name"));
+            bindPreferenceSummaryToValue(findPreference("owner_description"));
         }
 
         @Override
